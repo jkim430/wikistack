0446f93f376c7ed8bbfc017a48a35ce42b0581e5
thang239 <thanghnbk@gmail.com>
2015-06-24 17:09:06 -0400

    Edit tags nearly works

--- routes/index.js
+++ routes/index.js
@@ -28,6 +28,37 @@ router.post('/add/tag',function(req,res,next){
     res.redirect(url);
   })
 });
+// router.post('/delete/tag',function(req,res,next){
+//   var url = req.headers.referer
+//   var title = url.substr(url.lastIndexOf('/')+1)
+//   var newTags = req.body.tags.split(' ');
+//   models.Page.findOne({ url_name: title }, function(err, data) {
+//     if (err) throw Error;
+//     data.tags = data.tags.concat(newTags);
+//     data.save();
+//     res.redirect(url);
+//   })
+// });
+
+router.post('/edit',function(req,res,next){
+	var url = req.headers.referer;
+	var title = url.substr(url.lastIndexOf('/')+1)
+	models.Page.findOne({ url_name: title }, function(err, data) {
+    if (err) throw Error;
+    console.log(data.content);
+    res.render('edit_page',{title: 'EDIT PAGE '+data.title, pages:data});
+  })
+
+})
+
+router.get('/tags/:tag',function(req,res,next){
+	var tag = req.params.tag;
+	// console.log(req);
+	models.Page.find({tags:tag},function(err,data){
+		if(err) throw Error;
+		res.render('index',{title:'List pages with tag '+tag,pages:data});
+	})
+})
 
 // router.get('/add_page',function(req,res,next){
 // 	res.render('add_page',{title: 'ADD A PAGE'})
